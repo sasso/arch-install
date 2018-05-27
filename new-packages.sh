c3d8f840188278e593d4e266baa12c832041ed04
imba3r <imba3r@gmail.com>
2018-05-27 11:00:30 +0200

    Colorize pacman, improved package check.

--- new-packages.sh
+++ new-packages.sh
@@ -5,4 +5,8 @@ temp_file=$(mktemp)
 trap 'rm -f $temp_file' 0 2 3 15
 
 comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base base-devel xorg | sort) > "$temp_file"
-comm -23 "$temp_file" <(cat "conf.d/packages" "conf.d/packages-desktop" | sort)
+echo "New packages:"
+comm -23 "$temp_file" <(cat conf.d/packages* | grep -v '^base$\|^base-devel$\|^xorg$' | sort)  | grep -v '^vim$\|^gvim$'
+echo ""
+echo "Removed packages:"
+comm -23 <(cat conf.d/packages* | grep -v '^base$\|^base-devel$\|^xorg$' | sort) "$temp_file" | grep -v '^vim$\|^gvim$'
